b5d4a625584131598f8d84f5df16f1b1b9b2bdc4
David Haupt <davidkhaupt@gmail.com>
2015-09-17 06:43:01 -0400

    early views work

--- code/mysite/polls/views.py
+++ code/mysite/polls/views.py
@@ -1,8 +1,12 @@
 # polls/views.py
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("hello world, you're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
     
 def detail(request, question_id):
     return HttpResponse("you're looking at question %s." % question_id)
